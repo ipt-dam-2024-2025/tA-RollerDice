18cc26117c33212380c5aa25c18ba9052211ae28
jcnpereira <casimiro@ipt.pt>
2024-10-25 11:58:06 +0100

    dinâmica associada ao funcionamento do botão

--- app/src/main/java/pt/ipt/dam/roolerdice/MainActivity.kt
+++ app/src/main/java/pt/ipt/dam/roolerdice/MainActivity.kt
@@ -1,12 +1,18 @@
 package pt.ipt.dam.roolerdice
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var imagem: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,44 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // fazer referência ao Botão da app
+        var bt: Button = findViewById(R.id.btRollDice)
+        bt.setOnClickListener {
+            rodaDados()
+        }
     }
+
+    /**
+     * função que irá gerar um novo valor do dado
+     */
+    private fun rodaDados() {
+        // Algoritmo:
+        // 1. gerar um número aleatório entre [1;6]
+        // 2. associar a imagem respetiva ao ImageView
+        // 3. escrever o valor do nº aleatório no ecrã
+
+        // 1.
+        var numeroAleatorio = Random().nextInt(6) + 1 // valores entre 1 e 6
+
+        // 2.
+        var imagemParaImageView = when (numeroAleatorio) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        imagem = findViewById(R.id.imageViewDado)
+        imagem.setImageResource(imagemParaImageView)
+
+        // 3.
+        findViewById<TextView>(R.id.textViewValorDado).text = numeroAleatorio.toString()
+
+    }
+
+
 }
